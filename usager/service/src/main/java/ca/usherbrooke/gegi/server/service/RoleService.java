75428594bb5ff1a9cabaf7662bcb02517c1c6e38
Jordan2131 <lanj2131@usherbrooke.ca>
2023-06-05 14:17:13 -0400

    mdasls

--- usager/service/src/main/java/ca/usherbrooke/gegi/server/service/RoleService.java
+++ usager/service/src/main/java/ca/usherbrooke/gegi/server/service/RoleService.java
@@ -1,16 +1,20 @@
 package ca.usherbrooke.gegi.server.service;
 
+import ca.usherbrooke.gegi.server.business.Horaire;
 import ca.usherbrooke.gegi.server.business.Person;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.SecurityContext;
+
+import org.eclipse.microprofile.jwt.Claims;
 import org.eclipse.microprofile.jwt.JsonWebToken;
 
 @Path("/api")
@@ -21,7 +25,22 @@ public class RoleService {
     @Inject
     JsonWebToken jwt;
 
+  /*  @GET
+    @Path("/horaire/{sport_id}-{division_id}-{session_id}")
+    @PermitAll
+   // public JsonWebToken sport(){
+       // JsonObject text =
+       /*        { "numeroMatch" : 1,
+                "jourMatch" : "Jeudi",
+                "dateMatch" : "2023-06-01",
+                "heureMatch" : "19h00",
+                "equipe1" : "Les Gaulois",
+                "resultat" : "",
+                "equipe2" : "Les Pionniers",
+                "endroit" : "UDES" };
 
+      String json = ""*/
+ //   }*/
     @GET
     @Path("/teacher")
     @RolesAllowed({"enseignant"})
